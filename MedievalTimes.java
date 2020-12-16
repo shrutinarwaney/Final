123ea8ac75df84250b6c6684e961aa52b694d205
Shruti Narwaney <shruti.narwaney@gmail.com>
2020-12-15 22:24:28 -0700

    Add menu and some functionality

---
+++ MedievalTimes.java
@@ -0,0 +1,89 @@
+import java.util.*;
+import java.io.*;
+
+public class MedievalTimes {
+
+	Scanner userInput = new Scanner( System.in );
+	public static void main(String[] args) {
+		
+		System.out.println( "Welcome to medieval times!" );
+
+		while( true ) {
+
+			showMenu();
+			System.out.print( "Enter option: " );
+			int userOption = userInput.nextInt();
+
+			switch( userOption ) {
+
+				case 1:
+					makeParty();
+					break;
+				case 2:
+					validate();
+					break;
+				case 3:
+					reroll();
+					break;
+				case 4:
+					System.exit( 1 );
+				default:
+					System.out.println( "Not a valid input" );
+					break;
+
+			}
+
+		}
+
+	}
+
+	 public static void showMenu() {
+        System.out.println( "\nCreate new game (1) \nValidate/load save file (2) \nReroll existing character (3) \nQuit (4)" );  
+    }
+
+    public static void makeParty() {
+
+    	Party party = new Party();
+    	System.out.print( "What file to save to? (include .txt): " );
+    	String fileName = userInput.nextLine();
+    	try {
+    		party.setWriteFile( fileName );
+    		System.out.println( "Game is created!" );
+    	}
+    	catch( Exception e ) {
+    		System.out.println( "oops something went wrong" );
+    	}
+
+    }
+
+    public static void validate() {
+
+    	System.out.print( "Enter a file to validate: " );
+    	String fileName = userInput.nextLine();
+    	try {
+    		Party test = new Party( fileName );
+    		System.out.println( "Validated" );
+    	}
+    	catch( IOException e ) {
+    		System.out.println( "Not a file" );
+    	}
+    	catch( Exception e ) {
+    		System.out.println( "Not a medieval times file" );
+    	}
+
+    }
+
+    public static void reroll() {
+
+    	System.out.print( "Enter the file name where the game is: " );
+    	String fileName = userInput.nextLine();
+    	try {
+    		Party test = new Party( fileName );
+    		System.out.print( "Enter the name of the chatacter to randomize: " );
+    		String characterName = userInput.nextLine();
+    		
+    	}
+
+    }
+
+}
---
+++ Party.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Party {
+	
+	private final static int partySize = 4;
+	private Character [] players = new Character[ 4 ];
+	private String partyName;
+	private Scanner fileScanner;
+	private Scanner userInput = new Scanner( System.in );
+	PrintWriter out;
+
+	public Party( String partyName, Character [] players ) {
+		
+		this.partyName = partyName;
+		if( players.length <= partySize ) {
+			this.players = players;
+		}
+	
+	}
+
+	public Party( String fileName ) throws Exception {
+
+		fileScanner = new Scanner( new File( fileName ) );
+		parseFile();
+
+	}
+
+	public void setWriteFile( String fileName ) {
+		out = new PrintWriter( new File( fileName ) );
+		out.println( partyName );
+		for( Character player : players ) {
+			out.println( player );
+		}
+		out.close();
+	}
+
+	public void parseFile() throws Exception {
+		this.partyName = fileScanner.nextLine();
+		for( int i = 0; i < partySize; i++ ) {
+			players[i] = Character.parseLine( fileScanner.nextLine() );
+		}
+	}
+
+	public boolean charReroll( String name ) {
+		for( Character player : players ) {
+			if( player.getName().equals(name) ) {
+				player.
+			}
+		}
+	}
+
+}
